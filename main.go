563b28645511fad61e1bec45e6364bef282aab7f
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-09-08 07:51:00 +0100

    deploy

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"log"
 	"net/http"
 	"os"
-
+  "time"
+  "io"
 	"github.com/gomodule/redigo/redis"
 )
 
 var redisPool *redis.Pool
 
 func incrementHandler(w http.ResponseWriter, r *http.Request) {
+
 	conn := redisPool.Get()
 	defer conn.Close()
 
@@ -21,18 +23,35 @@ func incrementHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error incrementing visitor counter", http.StatusInternalServerError)
 		return
 	}
-	log.Printf(w, "Visitor number: %d", counter)
+	fmt.Fprintf(w, "Visitor number: %d", counter)
+	log.Printf("Visitor number: %d", counter)
+
+	time.Sleep(time.Second)
+	resp, err := http.Get("http://visit-counter:8080")
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	log.Printf(string(body))
+
 }
 
 func main() {
-	redisHost := os.Getenv("REDISHOST")
-	redisPort := os.Getenv("REDISPORT")
-	redisAddr := fmt.Sprintf("%s:%s", redisHost, redisPort)
+
+	log.SetOutput(os.Stderr)
+
+	redisAddr := os.Getenv("primary_endpoint_address")
+  redisToken := os.Getenv("auth_token")
+  redisPort := os.Getenv("redis_port")
+  if redisPort == "" {
+    redisPort = "6379"
+	}
+	redisConn := fmt.Sprintf("%s:%s", redisAddr, redisPort)
 
 	const maxConnections = 10
 	redisPool = &redis.Pool{
 		MaxIdle: maxConnections,
-		Dial:    func() (redis.Conn, error) { return redis.Dial("tcp", redisAddr) },
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", redisConn, redis.DialPassword(redisToken), redis.DialUseTLS(true))
+		},
 	}
 
 	http.HandleFunc("/", incrementHandler)
@@ -45,4 +64,5 @@ func main() {
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
+
 }
